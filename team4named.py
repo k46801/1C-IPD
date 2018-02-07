552d1b67ad7e936abe9accaa85187d525615d583
MarissaBenavidez <36002929+MarissaBenavidez@users.noreply.github.com>
2018-02-07 08:05:13 -0700

    tram 4 name
    marissa

--- team4named.py
+++ team4named.py
@@ -9,6 +9,8 @@
 team_name = 'Green_Pineapple' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
+    
+def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
