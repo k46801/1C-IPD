0c9b98c3e45fef5453d27281208b5f84e3c003ae
FrankHoyle <36162404+FrankHoyle@users.noreply.github.com>
2018-02-07 08:16:18 -0700

    team 6 name change
    Frank

--- team6.py
+++ team6.py
@@ -3,10 +3,10 @@
 #     team_name: a string
 #     strategy_name: a string
 #     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
+#     move: A function that returns 'c' or 'b'0
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Tide' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
